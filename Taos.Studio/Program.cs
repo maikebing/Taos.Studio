ddd73de53620051ef1700c6e319effa065d2b7b6
MysticBoy <mysticboy@live.com>
2020-06-04 01:21:37 +0800

    升级taos 以便 配合ILMerge

--- Taos.Studio/Program.cs
+++ Taos.Studio/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using NativeLibraryManager;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
