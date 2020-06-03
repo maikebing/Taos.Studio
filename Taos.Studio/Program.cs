d4b968553e444ee07de0def0b22067407d6340ee
MysticBoy <mysticboy@live.com>
2020-06-03 23:50:18 +0800

    兼容高分屏

--- Taos.Studio/Program.cs
+++ Taos.Studio/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +9,29 @@ namespace Taos.Studio
 {
     static class Program
     {
+        [DllImport("Shcore.dll")]
+        static extern int SetProcessDpiAwareness(int PROCESS_DPI_AWARENESS);
+
+        // According to https://msdn.microsoft.com/en-us/library/windows/desktop/dn280512(v=vs.85).aspx
+        private enum DpiAwareness
+        {
+            None = 0,
+            SystemAware = 1,
+            PerMonitorAware = 2
+        }
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
+            try
+            {
+                SetProcessDpiAwareness((int)DpiAwareness.SystemAware);
+            }
+            catch (Exception)
+            {
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm(args.Length == 0 ? null : new Maikebing.Data.Taos.TaosConnectionStringBuilder( args[0])));
